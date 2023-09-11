faf71c7ea4cbd29ac72d91563012f4f32e87c962
ThisSurya <suryamichael09@gmail.com>
2023-09-11 07:31:41 -0700

    restaurant api

---
+++ littlelemon/restaurant/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Booking
+from .models import Menu
+
+
+# superuser
+# admin
+# adminlittle@lemon
+# admin@123!@
+
+# Register your models here.
+admin.site.register(Booking)
+admin.site.register(Menu)
